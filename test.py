0113640359f3cc3a2b753534c9d22c39ec677e5d
Flo <floortner@users.noreply.github.com>
2021-12-07 01:40:50 +0100

    More refactoring

--- test.py
+++ test.py
@@ -15,17 +15,14 @@ class prop:
         self.sqm = sqm
     
     def __str__(self):
-        return "id %s, %s EUR, %s m2" % (self.id, self.price, self.sqm)
+        return "id: %s, %s EUR, %s m2" % (self.id, self.price, self.sqm)
 
 class scraperrun:
     def __init__(self, start_url):
-        self.start_url = start_url
-        self.ts_start = None
+        self.props = {}                 # store id:prop pairs
+        self.start_url = start_url      # willhaben url to start run
+        self.ts_start = None            # init defaults ...
         self.ts_end = None
-
-        self.props = {
-            '123' : prop('1', '', '', '', ''),
-            '234' : prop('2', '', '', '', '')}
         
     def __str__(self):
         return f'url: {self.start_url} started: {self.ts_start} props: {len(self.props)}'
@@ -36,26 +33,30 @@ class scraperrun:
         i = 0
         res = None
 
-        next_url = sr.start_url
-        split_url = urlsplit(sr.start_url)
+        next_url = self.start_url
+        
+        # get base url, it's always https://www.willhaben.at ;-)
+        split_url = urlsplit(self.start_url)
         base_url = f'{split_url.scheme}://{split_url.netloc}'
         
         while i < max_results:
 
             if res is None:
+                # get result page
                 res = requests.get(next_url)
                 if res.status_code != 200:
                     print(f'Cannot get {next_url}')
                     exit(1)
 
+                # parse all urls from the Javascript section
                 urls = re.findall('\"\/iad\/immobilien\/d\/.*?\/\"', res.text) 
                 
                 for u in urls:
                     url = base_url + u[1:-1]
                     try:
-                        prop = scraperrun.crawl_page(url)
-                        self.props[prop.id] = prop
-                        print(prop)
+                        p = scraperrun.crawl_page(url)
+                        self.props[p.id] = p
+                        print(p)
                     except:
                         print(f'Cannot parse {u}')
 
@@ -74,6 +75,16 @@ class scraperrun:
     
     def end_run(self):
         self.ts_end = datetime.datetime.now()
+    
+    def write_json(self, output_dir):
+        d = datetime.datetime.now()
+        fname = f'{output_dir}/willhaben_{d.year}-{d.month}-{d.day}_{d.hour}-{d.minute}.json'
+        
+        json_file = open(fname, 'w')
+        str = jsonpickle.encode(self)
+        json_file.write(str)
+        
+        return fname
 
     # crawl one page and return property object
     def crawl_page(url):
@@ -106,15 +117,15 @@ class scraperrun:
         else:
             return None
 
-    # get url of next page of search results
+    # get url of next page (pagination, click on 'next')
     def get_next_page(res):
         html = bs4.BeautifulSoup(res.text, 'html.parser')
 
-        """ # get total number or search results -> optional
-        total_results = html.find_all('h1', {'data-testid': 'result-list-title'})
-        total = re.findall('\d*', total_results[0].text.replace('.', ''))[0]
-        print(f'Total number of listings: {total}')
-        """       
+        # get total number or search results -> optional
+        #total_results = html.find_all('h1', {'data-testid': 'result-list-title'})
+        #total = re.findall('\d*', total_results[0].text.replace('.', ''))[0]
+        #print(f'Total number of listings: {total}')
+
         # return the URL of the 'next' page
         try:
             soup = html.find_all('a', {'data-testid': 'pagination-top-next-button'})
@@ -123,7 +134,24 @@ class scraperrun:
         except:
             return None
 
-    # convert string containing EUR currency to int (hack, to be improved)
+    # stats
+    def print_stats(self):
+        total_price = 0
+        total_squarefeet = 0
+        
+        for p in self.props:
+            total_price = total_price + self.props[p].price
+            total_squarefeet = total_squarefeet + self.props[p].sqm
+        
+        no_objects = len(self.props)
+        duration = self.ts_end - self.ts_start
+        
+        print(f'Pages crawled: {no_objects} [{duration}]')
+        print(f'Avg price: {(total_price/no_objects):.2f}')
+        print(f'Avg m2: {(total_squarefeet/no_objects):.2f}')
+        print(f'Price per m2: EUR {(total_price/total_squarefeet):.2f}')
+    
+    # convert string containing EUR currency to int (ugly, to be improved)
     def string_to_int(str):
         result = 0
         if ',' in str:
@@ -136,8 +164,16 @@ class scraperrun:
 if __name__ == "__main__":
     
     sr = scraperrun('https://www.willhaben.at/iad/immobilien/mietwohnungen/oberoesterreich/linz?page=1&rows=100')
-    sr.start_run(5)
+    sr.start_run(1)
     sr.end_run()
+    sr.print_stats()
+    #sr.write_json('.')
+    
+    sr.props = {}
+    sr.props['345'] = prop('3', '', '', '', '')
+    str = jsonpickle.encode(sr)
+    print(str)
+    
     exit(1)
 
     sr = scraperrun('willhaben.at')
